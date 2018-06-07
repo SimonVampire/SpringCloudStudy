13eace569729cd0c29909c23c1519c96db0abc42
SimonVampire <345623958@q.com>
2018-06-07 15:27:53 +0800

    feign调用方式

---
+++ service-consumer-feign/src/main/java/cn/sevendream/springcloudstudy/serviceconsumerfeign/controller/HiController.java
@@ -0,0 +1,19 @@
+package cn.sevendream.springcloudstudy.serviceconsumerfeign.controller;
+
+import cn.sevendream.springcloudstudy.serviceconsumerfeign.service.HiSchedualService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    HiSchedualService schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
